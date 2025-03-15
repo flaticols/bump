368a5b445700e2d6919d356397ad88f37e0a8511
Denis Panfilov <gh@flaticols.dev>
2025-03-15 10:38:47 +0100

    initial
    Signed-off-by: Denis Panfilov <gh@flaticols.dev>

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/fatih/color"
+)
+
+var (
+	version   = "dev"
+	buildTime = "unknown"
+)
+
+type cliOpts struct {
+	repoPath string
+	version  bool
+	verbose  bool
+}
+
+func main() {
+	opts := cliOpts{}
+	flag.BoolVar(&opts.version, "version", false, "Print version information")
+	flag.BoolVar(&opts.verbose, "verbose", false, "Print verbose information")
+	flag.Parse()
+
+	if opts.version {
+		fmt.Println(version)
+	}
+
+	if opts.repoPath != "" {
+		if err := setBumpWd(opts.repoPath); err != nil {
+			color.Red("Failed to change to repository directory")
+			return
+		}
+	}
+
+	// if ok, err := isDefaultBranch(); err != nil {
+	// 	color.Red("Failed to check branch")
+	// 	return
+	// } else if !ok {
+	// 	color.Red("Not on default branch")
+	// 	fmt.Printf("Switch to default branch (%s) first before run bump tool\n", strings.Join(defaultBranches, ","))
+	// 	return
+	// }
+
+	// dirt, err := checkLocalChanges()
+	// if err != nil {
+	// 	color.Red("Failed to check local changes")
+	// 	return
+	// }
+	// if dirt {
+	// 	color.Red("Local changes detected")
+	// 	fmt.Println("Commit your changes first before run bump tool")
+	// }
+
+	// needFetch, err := checkRemoteChanges()
+	// if err != nil {
+	// 	color.Red("Failed to check remote changes")
+	// 	if opts.verbose {
+	// 		fmt.Println(err)
+	// 	}
+	// 	return
+	// }
+
+	// if needFetch {
+	// 	color.Red("Remote changes detected")
+	// 	fmt.Println("Fetch remote changes first before run bump tool")
+	// }
+	//
+
+	ltag, initVer, err := getLatestGitTag()
+	if err != nil {
+		color.Red("Failed to get latest git tag")
+		if opts.verbose {
+			fmt.Println(err)
+		}
+		return
+	}
+
+	var nextVer *semver.Version
+
+	green := color.New(color.FgGreen).SprintFunc()
+	if !initVer {
+		curVer, err := parseTag(ltag)
+		if err != nil {
+			color.Red("Failed to parse latest git tag")
+			if opts.verbose {
+				fmt.Println(err)
+			}
+			return
+		}
+		nv := curVer.IncPatch()
+		nextVer = &nv
+		blue := color.New(color.FgBlue).SprintFunc()
+		fmt.Printf("Version bump %s => %s\n", blue(fmt.Sprintf("v%s", curVer.String())), green(fmt.Sprintf("v%s", nextVer.String())))
+	} else {
+		nextVer = semver.MustParse("0.0.1")
+		fmt.Printf("First version %s\n", green(fmt.Sprintf("v%s", nextVer.String())))
+	}
+
+	fmt.Println("Git tag created")
+	fmt.Println("Git tag pushed")
+}
